ce52f07c090a35f3dec0184aa5899a73aee8c8e0
paulo.bressan <paulo.bressan@luizalabs.com>
2019-02-25 13:36:45 -0300

    Criando um redirecionamento para rota que chamou o login. Utilizando validação na lacuna do html para validar se a variavel esta nula.

--- jogoteca.py
+++ jogoteca.py
@@ -20,7 +20,7 @@ lista = [pokemon, supermario]
 # definindo uma rota, por padrão GET
 @app.route('/')
 def inicio():
-    #Utilizando a função render_template do flask para renderizar paginas html dinamicas(Jinja2)
+    # Utilizando a função render_template do flask para renderizar paginas html dinamicas(Jinja2)
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
@@ -28,7 +28,7 @@ def inicio():
 def novo():
     # validação se o usuario esta logado na sessão
     if 'usuario_logado' not in session or session['usuario_logado'] is None:
-        return redirect('/login')
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 
@@ -43,26 +43,35 @@ def criar():
     lista.append(jogo)
     return redirect('/')
 
+
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    # capturando uma query string ?proxima=VALOR
+    proxima = request.args.get('proxima')
+    # passando para o template para onde vamos ir depois de autenticar
+    return render_template('login.html', proxima=proxima)
+
 
 @app.route('/autenticar', methods=['POST'])
 def autenticar():
     if 'mestra' == request.form['senha']:
-        #criando uma sessão. Essa sessão não é uma sessão do lado do servidor e ela é armazenada no cookie no navegador
+        # criando uma sessão. Essa sessão não é uma sessão do lado do servidor e ela é armazenada no cookie no navegador
         session['usuario_logado'] = request.form['usuario']
         # O flash é um helper para exibir uma mensagem no html
         flash(f'{request.form["usuario"]} logou com sucesso')
-        return redirect('/')
+        # Proxima pagina
+        proxima_pagina = request.form['proxima']
+        return redirect(f'/{proxima_pagina}')
     else:
         flash('Não logado, tente novamente')
         return redirect('/login')
+
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Usuário deslogado com sucesso')
     return redirect('/')
+
 
 app.run(host='0.0.0.0', port=5000, debug=True)
