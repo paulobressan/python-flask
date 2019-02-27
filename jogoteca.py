ff76659f0083bb94d773e981228b94ccc2bcf17c
paulobressan <paulo.bressan@outlook.com>
2019-02-26 21:21:32 -0300

    Implementando o mysql conector e realizando consultas.

--- jogoteca.py
+++ jogoteca.py
@@ -6,7 +6,8 @@ app.secret_key = 'flask-app'
 
 
 class Jogo:
-    def __init__(self, nome, categoria, console):
+    def __init__(self, id, nome, categoria, console):
+        self.id = id
         self.nome = nome
         self.categoria = categoria
         self.console = console
